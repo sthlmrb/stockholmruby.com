0d22f986710fec4abe7875e606ee6d240338c344
Henrik Nyh <henrik@nyh.se>
2013-09-17 19:01:13 +0200

    Sass, Slim, some styling.

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
 
+get "/styles.css" do
+  scss :styles
+end
+
 get "/" do
-  "Hallå Stockholm hur är läget?!"
+  slim :index
 end
