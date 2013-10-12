fc94e1c84a74190c0813ea5387162d373c08758d
Henrik Nyh <henrik@nyh.se>
2013-10-12 19:01:28 +0200

    Replace ugly JS 'upcoming' with Ruby.

--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@ require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
 
+require_relative "meetup"
+
 get "/styles.css" do
   scss :styles
 end
 
 get "/" do
-  slim :index
+  slim :index, locals: {
+    next_event: Meetup.upcoming(1).first
+  }
 end
 
 get "/organize" do
---
+++ meetup.rb
@@ -0,0 +1,32 @@
+require "open-uri"
+require "json"
+require "timeout"
+require "ostruct"
+
+class Meetup
+  class Result < OpenStruct
+    def time
+      Time.at(super / 1000)
+    end
+  end
+
+  def self.upcoming(count)
+    key  = ENV["MEETUP_KEY"]
+    url  = "https://api.meetup.com/2/events?&sign=true&group_urlname=sthlmrb&page=#{count}&key=#{key}"
+    json = Timeout.timeout(5) { open(url).read }
+    data = JSON.parse(json)
+
+    data["results"].map { |x|
+      Result.new(
+        name: x["name"],
+        url:  x["event_url"],
+        time: x["time"],
+        description: x["description"],
+        venue_name:    x["venue"]["name"],
+        venue_address: x["venue"]["address_1"]
+      )
+    }
+  rescue TimeoutError
+    []
+  end
+end
