5d1708ee741ed978cac8f82d4d42ffdb559e1186
Henrik Nyh <henrik@nyh.se>
2013-10-15 21:44:58 +0200

    Spec organize and work pages.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,9 +3,23 @@ require "capybara/rspec"
 require_relative "../app"
 Capybara.app = Sinatra::Application.new
 
-describe "Start page", type: :feature do
-  it "renders happily" do
+feature "Integrated pages", type: :feature do
+  scenario "Visiting start page" do
     visit "/"
     expect(page).to have_content("Stockholm Ruby")
+  end
+
+  scenario "Visiting 'organize' page" do
+    visit "/"
+    click_link "Organize"
+    expect(current_path).to eq("/organize")
+    expect(page).to have_content("Organize")
+  end
+
+  scenario "Visiting 'work' page" do
+    visit "/"
+    click_link "Find a job"
+    expect(current_path).to eq("/work")
+    expect(page).to have_content("Work")
   end
 end
