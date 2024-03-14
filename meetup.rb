fc954b93fa51446ca3aa1a69841093e322a851d5
Andreas Zecher <andreas@polylists.com>
2024-03-14 22:20:12 +0100

    Update homepage and links on learn page

--- meetup.rb
+++ meetup.rb
@@ -6,12 +6,12 @@ class Meetup
   def self.upcoming
     [
       Result.new(
-        name: "Ruby GAME SHOW meetup",
-        url:  "https://www.cloudamqp.com/blog/ruby-game-show-meetup.html",
-        time: Time.parse("2023-03-16 18:00 CEST"),
-        description: "We are excited to welcome you to the Ruby Game Show Meetup in Stockholm! Join us at 6 pm on March 16 for a fantastic evening with a mix of games, talks, food, and drinks. In addition, you can meet other developers and make new connections in the industry.",
-        venue_name:    "Gasverket/United Spaces",
-        venue_address: "Torsgatan 25, Stockholm",
+        name: "Ruby Meetup with Hemnet",
+        url: "https://rubymeetup.confetti.events/signup",
+        time: Time.parse("2024-03-13 17:30 CEST"),
+        description: "A sparkling event hosted by Ruby fans for Ruby fans in a cozy environment with 🍕 and🧋. Listen to our colleague Olov talking about 'The Journey to a new Map' and Youssef dive into image postprocessing in Ruby.",
+        venue_name: "Hemnet",
+        venue_address: "Sveavägen 9",
       )
     ]
   end
