39e69347c94a8147b83c1c5a5f124a049b95459f
Kim Burgestrand <kim@burgestrand.se>
2024-10-15 11:36:27 +0200

    Add most recent meetup event to past events
    We used to have a meetup.com integration, it was removed in 76fb414b082e34ae0fe33b561b7661b1cf91fe6e.
    
    I didn't bother re-adding it just now.

--- meetup.rb
+++ meetup.rb
@@ -13,6 +13,19 @@ Meetup = Data.define(:name, :url, :time, :description, :venue_name, :venue_addre
   def self.all
     [
       new(
+        name: "Cozy Ruby Meetup at Hemnet",
+        url: "https://www.meetup.com/sthlmrb/events/303483996/",
+        time: Time.parse("2024-09-25 17:30 CEST"),
+        description: <<~DESCRIPTION,
+        Welcome to Ruby Meetup at Hemnet office at Sveavägen 9.
+          After Baltic ruby we have talked about a need to have a place of
+          gathering for community. Hemnet office can be that place with some
+          home rules and keeping it low-key.
+        DESCRIPTION
+        venue_name: "Hemnet",
+        venue_address: "Sveavägen 9",
+      ),
+      new(
         name: "Ruby Meetup with Hemnet",
         url: "https://rubymeetup.confetti.events/signup",
         time: Time.parse("2024-03-13 17:30 CEST"),
